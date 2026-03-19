d0fd20012d2a7f41359d4d4a4ce304f4fcba9722
Avronil <basuavronil65@gmail.com>
2026-03-19 11:13:39 +0530

    Update even_function.v

--- even_function.v
+++ even_function.v
@@ -1,4 +1,21 @@
 module test;
+  string b;
+  function int odd_even (input int a);
+    int temp = 0;
+    begin 
+      temp = a;
+      if ( temp % 2 == 0)
+        $display (" a is even");
+      else 
+        $display (" a is odd");
+    end
+  endfunction
+    initial begin
+      b = odd_even (10);
+    end
+endmodule
+
+/*module test;
   int b;
   function int even ( input int a );
     if ( a % 2 == 0)
@@ -9,4 +26,4 @@ module test;
   initial begin
     b = even (.a(100));
   end
-  endfunction
+  endfunction*/
